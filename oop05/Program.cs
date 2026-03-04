5c4fb86a8da25cc4f5c51bc943bc54e1c65329aa
ahmedmostafa500 <plmncar500@gmail.com>
2026-03-04 17:29:52 +0200

    question1

--- oop05/Program.cs
+++ oop05/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace oop05
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using static System.Collections.Specialized.BitVector32;
+
+namespace oop05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-     
+            #region part1
+            #region question1
+            //What is an Interface in C#?
+            //An interface in C# is a reference type that defines a contract (a set of methods, properties, events, or indexers) 
+            //that a class must implement.
+            //It contains only declarations, not implementation
+
+            //Why Do We Use Interfaces Instead of Depending on Concrete Classes ?
+            //Achieve loose coupling
+            //Improve testability
+            //Support extensibility
+            //Follow SOLID principles
+
+            //Mention at least three benefits of using interfaces:
+            //1-Loose Coupling
+            //2- Better Testability(Mocking)
+            //3- Extensibility(Open / Closed Principle)
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
