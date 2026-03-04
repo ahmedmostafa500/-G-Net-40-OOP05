843fa014dad7ff0d298712ccbe7c07ca52692503
ahmedmostafa500 <plmncar500@gmail.com>
2026-03-04 17:50:01 +0200

    question3

--- oop05/Program.cs
+++ oop05/Program.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.IO;
+using System.Net;
 using System.Runtime.Intrinsics.X86;
 using System.Security.AccessControl;
+using System.Threading.Channels;
+using System.Xml.Linq;
 using static System.Collections.Specialized.BitVector32;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -45,24 +49,56 @@ namespace oop05
             //We use Explicit Interface Implementation.
 
 
-         //   c) Can we call translator.Greet() directly ?
-         //  No, we cannot.
-         //  Why?
-         // Because explicit interface methods :
-         //Are not public
-         //Are only accessible through the interface reference
+            //   c) Can we call translator.Greet() directly ?
+            //  No, we cannot.
+            //  Why?
+            // Because explicit interface methods :
+            //Are not public
+            //Are only accessible through the interface reference
 
-        // How to call each version?
+            // How to call each version?
 
-        // Translator translator = new Translator();
+            // Translator translator = new Translator();
 
-        //IEnglishSpeaker english = translator;
+            //IEnglishSpeaker english = translator;
 
-        //english.Greet();   // Prints: Hello
+            //english.Greet();   // Prints: Hello
 
-        // IArabicSpeaker arabic = translator;
+            // IArabicSpeaker arabic = translator;
 
-        //arabic.Greet();    // Prints: Ahlan
+            //arabic.Greet();    // Prints: Ahlan
+            #endregion
+            #region question3
+            //Difference Between Shallow Copy and Deep Copy
+            //1-Shallow Copy:
+            //A shallow copy creates a new object, but:
+            //Value - type fields: copied normally(new independent values)
+            //Reference - type fields: the reference(address) is copied, not the actual object
+            //So both objects point to the same referenced object in memory.
+
+            //2-Deep Copy:
+            //A deep copy creates a completely independent copy:
+            //Value - type fields → copied
+            //Reference - type fields → new objects are created
+            //So the copied object does NOT share any internal references with the original.
+            //Each object is fully independent.
+
+            // When Do We Use Each One?
+            //Use Shallow Copy When:
+            //The object contains only value types
+            //Or reference fields are immutable(like string)
+            //Or you intentionally want shared references
+            //For performance(it’s faster and uses less memory)
+
+            //Use Deep Copy When:
+            //The object contains mutable reference-type fields
+            //You want a completely independent object
+            //You don’t want changes in one object to affect the other
+
+            //What Is the Risk of Using Shallow Copy with Reference-Type Fields?
+            //The main risk is:
+            //Unintended side effects.
+            //If both objects share the same referenced object, modifying it in one object will affect the other.
             #endregion
 
             #endregion
